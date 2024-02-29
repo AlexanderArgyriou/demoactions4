7c3c112cfae98a0906726839dc0e54a231ed6140
Alex Argyriou <Alex.argyriou@vodafone.com>
2024-02-29 11:23:09 +0200

    Initial commit

---
+++ src/main/java/com/argyriou/demoactions4/Demoactions4Application.java
@@ -0,0 +1,13 @@
+package com.argyriou.demoactions4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demoactions4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demoactions4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/argyriou/demoactions4/Demoactions4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.argyriou.demoactions4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demoactions4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
